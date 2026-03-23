e36d3eec9dd48dd6db52d73f70f5a5bc3cb682fc
József Bartók <jbartok@gradle.com>
2026-03-23 11:07:56 +0200

    Switch ResidualProgramCompiler to BTA

--- platforms/core-configuration/kotlin-dsl/build.gradle.kts
+++ platforms/core-configuration/kotlin-dsl/build.gradle.kts
@@ -61,8 +61,6 @@ dependencies {
     implementation(projects.fileTemp)
     implementation(projects.files)
     implementation(projects.functional)
-    implementation(projects.hashingServices)
-    implementation(projects.io)
     implementation(projects.logging)
     implementation(projects.messaging)
     implementation(projects.projectFeaturesApi)
@@ -91,9 +89,6 @@ dependencies {
         isTransitive = false
     }
     implementation(libs.kotlinScriptingJvm) {
-        isTransitive = false
-    }
-    implementation(libs.kotlinScriptingJvmHost) {
         isTransitive = false
     }
     implementation(libs.kotlinScriptingCompilerEmbeddable) {
