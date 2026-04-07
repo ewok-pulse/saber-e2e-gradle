8d07d95eba9109fc8569743d3d263a479f316b6a
József Bartók <jbartok@gradle.com>
2026-04-07 11:25:52 +0300

    Adapt code to version bump

--- platforms/core-configuration/kotlin-dsl/build.gradle.kts
+++ platforms/core-configuration/kotlin-dsl/build.gradle.kts
@@ -79,6 +79,7 @@ dependencies {
 
     implementation(libs.asm)
     implementation(libs.jetbrainsAnnotations)
+    implementation(libs.kotlinBuildToolsApi)
     implementation(libs.kotlinReflect)
     implementation(libs.slf4jApi)
 
