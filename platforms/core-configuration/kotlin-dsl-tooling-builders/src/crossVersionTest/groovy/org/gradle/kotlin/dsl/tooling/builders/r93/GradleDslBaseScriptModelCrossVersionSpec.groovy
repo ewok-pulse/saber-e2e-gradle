88e1dae589e6d9d987578415e83d5bf26bfea1c1
József Bartók <jbartok@gradle.com>
2026-04-15 10:29:57 +0300

    Adjust test to produce a single compilation error

--- platforms/core-configuration/kotlin-dsl-tooling-builders/src/crossVersionTest/groovy/org/gradle/kotlin/dsl/tooling/builders/r93/GradleDslBaseScriptModelCrossVersionSpec.groovy
+++ platforms/core-configuration/kotlin-dsl-tooling-builders/src/crossVersionTest/groovy/org/gradle/kotlin/dsl/tooling/builders/r93/GradleDslBaseScriptModelCrossVersionSpec.groovy
@@ -121,11 +121,11 @@ class GradleDslBaseScriptModelCrossVersionSpec extends AbstractKotlinScriptModel
         result.baseModel.kotlinDslBaseScriptModel != null
 
         where:
-        typeOfFailure | error                                    | expectedReportedError                            | apiType
-        "compilation" | "broken !!!"                             | /Script compilation error:\s+Line 1: broken !!!/ | ApiType.FETCH
-        "runtime"     | "throw RuntimeException(\"broken !!!\")" | /Settings file '.*?' line: 1\s+broken !!!/       | ApiType.FETCH
-        "compilation" | "broken !!!"                             | /Script compilation error:\s+Line 1: broken !!!/ | ApiType.GET_MODEL
-        "runtime"     | "throw RuntimeException(\"broken !!!\")" | /Settings file '.*?' line: 1\s+broken !!!/       | ApiType.GET_MODEL
+        typeOfFailure | error                                   | expectedReportedError                         | apiType
+        "compilation" | "broken"                                | /Script compilation error:\s+Line 1: broken/  | ApiType.FETCH
+        "runtime"     | "throw RuntimeException(\"broken\")"    | /Settings file '.*?' line: 1\s+broken/        | ApiType.FETCH
+        "compilation" | "broken"                                | /Script compilation error:\s+Line 1: broken/  | ApiType.GET_MODEL
+        "runtime"     | "throw RuntimeException(\"broken\")"    | /Settings file '.*?' line: 1\s+broken/        | ApiType.GET_MODEL
     }
 
     def "GradleDslBaseScriptModel can be obtained even after a project #typeOfFailure failure with #apiType"() {
@@ -148,11 +148,11 @@ class GradleDslBaseScriptModelCrossVersionSpec extends AbstractKotlinScriptModel
         result.baseModel.kotlinDslBaseScriptModel != null
 
         where:
-        typeOfFailure | error                                    | expectedReportedError                         | apiType
-        "compilation" | "broken !!!"                             | "A problem occurred configuring root project" | ApiType.FETCH
-        "runtime"     | "throw RuntimeException(\"broken !!!\")" | "A problem occurred configuring root project" | ApiType.FETCH
-        "compilation" | "broken !!!"                             | "A problem occurred configuring root project" | ApiType.GET_MODEL
-        "runtime"     | "throw RuntimeException(\"broken !!!\")" | "A problem occurred configuring root project" | ApiType.GET_MODEL
+        typeOfFailure | error                                   | expectedReportedError                         | apiType
+        "compilation" | "broken"                                | "A problem occurred configuring root project" | ApiType.FETCH
+        "runtime"     | "throw RuntimeException(\"broken\")"    | "A problem occurred configuring root project" | ApiType.FETCH
+        "compilation" | "broken"                                | "A problem occurred configuring root project" | ApiType.GET_MODEL
+        "runtime"     | "throw RuntimeException(\"broken\")"    | "A problem occurred configuring root project" | ApiType.GET_MODEL
     }
 
     static class FetchBaseModelAfterSettingsEvaluationAction implements BuildAction<FetchBaseModelLastActionResult>, Serializable {
